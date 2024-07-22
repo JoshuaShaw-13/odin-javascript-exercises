effd1b296644f997db57a194317c5f7ecc10aa0e
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-07-22 09:45:35 -0600

    add code to make sumAll work correctly

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) == "number" && typeof(num2) == "number" && num1 >= 0 && num2 >= 0 && num1%1 === 0 && num2%1 === 0) {
+        if (num2 >= num1) {
+            min = num1;
+            max = num2;
+        }
+        else {
+            min = num2;
+            max = num1;
+        }
+    
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR"
+    } 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+// console.log(typeof(7))
