13385d2444f1da75b6b7b9b1e4edd8411e624f60
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-07-22 09:13:18 -0600

    add code to make removeFromArray work correctly

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
+    let arr = arguments[0]
+    let content = []
+    for (let i = 1; i < arguments.length; i++) {
+        content.push(arguments[i])
+    }
+    //console.log(content)
+    //console.log(arr.length)
+    const limit = content.length
+
+    for (let i = 0; i < limit; i++) {
+        while (arr.includes(content[i])) {
+            let pos = arr.indexOf(content[i])
+            arr.splice(pos, 1); 
+        }
+    }
+    //console.log(arr)
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// removeFromArray([1, 2, 3, 4], 3, 2);
