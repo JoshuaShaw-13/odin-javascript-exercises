64b11e5da5b53b652d7c486d7f04afbdefeb8985
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-08-06 12:39:27 -0600

    finish fibonacci function to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS";
+    };
+    fib = [0,1];
+    if (num >= 2) {
+        let i = 2;
+        while (fib.length < num+1) {
+            fib.push(fib[i-1]+fib[i-2]);
+            i++;
+        }
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
