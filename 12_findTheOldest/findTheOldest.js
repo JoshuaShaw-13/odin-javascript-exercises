2344a927a72d8907bb7dc7e2c1beab5245079e36
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-08-06 14:11:22 -0600

    finish findTheOldest function to pass tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let cur_age = 0;
+    let old_age = 0;
 
+    for (i = 0; i < people.length; i++) {
+        let birth = people[i].yearOfBirth;
+        let death = people[i].yearOfDeath;
+        if (people[i].yearOfDeath == undefined) {
+            death = 2024;
+        };
+        cur_age = death - birth;
+        if (cur_age > old_age) {
+            old_age = cur_age;
+            oldest = i;
+        }
+    }
+
+    return people[oldest];
 };
 
 // Do not edit below this line
