2544f167d8ea928ad1600a487051fa9129ca6de4
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-08-06 11:55:08 -0600

    finish calculator functions to pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(num_arr) {
+	const sumOfNums = num_arr.reduce((total, currentNum) => {
+    return total + currentNum;
+  }, 0);
+  return sumOfNums;
 };
 
-const multiply = function() {
-
+const multiply = function(num_arr) {
+  const prodOfNums = num_arr.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
+  return prodOfNums;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let i = 1;
+  let total = 1;
+  while (i <= num) {
+    total *= i;
+    i++;
+  }
+  return total;
 };
 
 // Do not edit below this line
